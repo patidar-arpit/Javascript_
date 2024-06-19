623f7715283b798b5c94a94d788ae0a91d0ed08b
Arpit Patidar <arpitpatidar810985@gmail.com>
2024-06-19 15:52:26 +0530

    comparison of the datatypes with the help of operators

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,10 @@
+console.log(2>1);
+console.log(2==1);
+console.log("2" >1);  // Not give the accurate and predictable result on comparison of the different datatypes.
+
+
+//strict check --> ===
+//check the value as well as data type 
+
+console.log(2=="2"); // giive true  it will check only the values 
+console.log(2==="2"); //give false it will check value as well as data type of the values

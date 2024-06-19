b2c1961a3a588ca625d612ff8c800c70dab34371
Arpit Patidar <arpitpatidar810985@gmail.com>
2024-06-19 15:50:10 +0530

    data type basics

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //  this means -> treat all JS Code as newer version 
+
+let name = "arpit"
+let age = 24
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean
+//null -> standalone value ; Representaion of the empty value 
+//undefined -> variable is defined but its value is not defined
+//symbol -> to find uniquness of the component in React JS
+//object 
+
+
+//typeof is an operator used for type checking and returns the data type of the operand passed to it
+console.log(typeof  101);
+console.log(typeof  null); //null is type of the object
+console.log(typeof name);
